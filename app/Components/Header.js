19095b2ef76241df8cb46976bbd3529d93ee3629
Juarez <a1016060@bestbuy.com>
2022-05-06 11:21:01 -0500

    testing new image resource

--- app/Components/Header.js
+++ app/Components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import bbheader from '../../public/images/bestbuyheader.png';
 
 export default function Header() {
     return(
         <div>
             <header className="text-gray-600 body-font">
                 <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-                    <img src="./images/bestbuyheader.png" />
+                    <img src={bbheader} />
                     <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
                         <a className="mr-5 hover:text-gray-900" href="./">Home</a>
                         <a className="mr-5 hover:text-gray-900" href="./">Vendor List</a>
