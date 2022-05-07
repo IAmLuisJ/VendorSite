a47062bb70941acbc99868a92879983b5a478bd3
Luis Juarez <luisisthejuarez@gmail.com>
2022-05-07 15:33:34 -0400

    updated header font color

--- app/Components/Header.js
+++ app/Components/Header.js
@@ -2,13 +2,13 @@
 export default function Header() {
     return(
         <div>
-            <header className="text-gray-600 body-font">
-                <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-                    <img src="/images/bestbuyheader.png" alt="bannerImage"/>
-                    <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-                        <a className="mr-5 hover:text-gray-900" href="./">Home</a>
-                        <a className="mr-5 hover:text-gray-900" href="./">Vendor List</a>
-                        <a className="mr-5 hover:text-gray-900" href="./VendorDetail">Vendor Detail</a>
+            <header className="text-white body-font">
+                <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center bg-blue-700">
+                    <img src="/images/bestbuyheader.PNG" alt="bannerImage"/>
+                    <nav className="md:ml-auto flex flex-wrap items-center text-lg justify-center">
+                        <a className="mt-5 mr-5 hover:text-gray-900" href="./">Home</a>
+                        <a className="mt-5 mr-5 hover:text-gray-900" href="./">Vendor List</a>
+                        <a className="mt-5 mr-5 hover:text-gray-900" href="./VendorDetail">Vendor Detail</a>
                     </nav>
                 </div>
             </header>
