42813e922c83bfc13e0d33750e57bd92d7bb720f
Luis Juarez <luisisthejuarez@gmail.com>
2022-05-14 16:59:35 -0400

    adding required tags to form

--- app/routes/Admin.jsx
+++ app/routes/Admin.jsx
@@ -4,8 +4,7 @@ import { db } from "~/utils/db.server";
 
 
 async function newVendor(companyName, companyEmail, companyCode, userEmail, userName) {
-    //TODO: Update this create query
-    await db.company.create({
+    const newVend = await db.company.create({
         data: {
             name: companyName,
             email: companyEmail,
@@ -18,6 +17,9 @@ async function newVendor(companyName, companyEmail, companyCode, userEmail, user
             }
         },
     })
+
+    console.log("response from prisma", newVend);
+    return newVend;
 }
 
 async function updateVendor(companyId) {
@@ -40,14 +42,25 @@ export const action = async ({ request }) => {
     if (myaction == "AddCompany") {
         const companyName = form.get("companyName");
         const companyEmail = form.get("companyEmail");
+        const companyCode = form.get("companyCode");
+        const userName = form.get("name");
+        const userEmail = form.get("email");
         if (typeof companyName !== "string") {
             return json({ formError: "Form not submitted correctly" }, { status: 400 })
         }
 
-        newVendor(companyName);
+        console.log("Submitting new vendor");
+        console.log("company Name", companyName);
+        console.log("company Email", companyEmail);
+        console.log("company Code", companyCode);
+        console.log("user name", userName);
+        console.log("user email", userEmail);
+        const vend = await newVendor(companyName, companyEmail, companyCode, userEmail, userName);
+        console.log("response from action", vend);
+        return redirect(`/vendors/${vend.companyCode}`);
     }
 
-    return redirect(`/vendors/vendor.id`);
+
 }
 
 const Admin = () => {
@@ -65,35 +78,44 @@ const Admin = () => {
                             <div class="p-2 w-full">
                                 <div class="relative">
                                     <label for="companyName" class="leading-7 text-sm text-gray-400">Company Name</label>
-                                    <input id="companyName" name="companyName" class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-blue-500 focus:bg-gray-900 focus:ring-2 focus:ring-blue-900 h-12 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out" />
+                                    <input id="companyName" name="companyName" class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-blue-500 focus:bg-gray-900 focus:ring-2 focus:ring-blue-900 h-12 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out" required />
                                 </div>
                             </div>
                             <div class="p-2 w-full">
                                 <div class="relative">
                                     <label for="companyEmail" class="leading-7 text-sm text-gray-400">Company Email</label>
-                                    <input id="companyEmail" name="companyEmail" class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-blue-500 focus:bg-gray-900 focus:ring-2 focus:ring-blue-900 h-12 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out" />
+                                    <input id="companyEmail" name="companyEmail" class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-blue-500 focus:bg-gray-900 focus:ring-2 focus:ring-blue-900 h-12 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out" required />
                                 </div>
+                            </div>
+                            <div class="p-2 w-full">
+                                <div class="relative">
+                                    <label for="companyCode" class="leading-7 text-sm text-gray-400">Company 3 Digit Code</label>
+                                    <input id="companyCode" name="companyCode" class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-blue-500 focus:bg-gray-900 focus:ring-2 focus:ring-blue-900 h-12 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out" required />
+                                </div>
+                            </div>
+                            <div class="flex flex-col text-center w-full mb-12">
+                                <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Create a User with the company</p>
                             </div>
                             <div class="p-2 w-1/2">
                                 <div class="relative">
                                     <label for="name" class="leading-7 text-sm text-gray-400">Name</label>
-                                    <input type="text" id="name" name="name" class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-blue-500 focus:bg-gray-900 focus:ring-2 focus:ring-blue-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                                    <input required type="text" id="name" name="name" class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-blue-500 focus:bg-gray-900 focus:ring-2 focus:ring-blue-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                                 </div>
                             </div>
                             <div class="p-2 w-1/2">
                                 <div class="relative">
                                     <label for="email" class="leading-7 text-sm text-gray-400">Email</label>
-                                    <input type="email" id="email" name="email" class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-blue-500 focus:bg-gray-900 focus:ring-2 focus:ring-blue-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                                    <input required type="email" id="email" name="email" class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-blue-500 focus:bg-gray-900 focus:ring-2 focus:ring-blue-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                                 </div>
                             </div>
-                            <div class="p-2 w-full">
+                            {/* <div class="p-2 w-full">
                                 <div class="relative">
                                     <label for="message" class="leading-7 text-sm text-gray-400">Message</label>
                                     <textarea id="message" name="message" class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-blue-500 focus:bg-gray-900 focus:ring-2 focus:ring-blue-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
                                 </div>
-                            </div>
+                            </div> */}
                             <div class="p-2 w-full">
-                                <button type="submit" class="flex mx-auto text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">Button</button>
+                                <button type="submit" class="flex mx-auto text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">Create</button>
                             </div>
                         </div>
                     </div>
