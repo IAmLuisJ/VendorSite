16f43cd030b87e6dd45fc40111aa030cd822dd48
Luis Juarez <luisisthejuarez@gmail.com>
2022-05-15 00:01:46 -0400

    rendering vendor List from findMany query

--- app/Components/FeedbackForm.jsx
+++ app/Components/FeedbackForm.jsx
@@ -1,38 +1,38 @@
 
 const FeedBackForm = () => {
     return (
-        <section class="text-gray-600 body-font relative">
-            <div class="container px-5 py-24 mx-auto">
-                <div class="flex flex-col text-center w-full mb-12">
-                    <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Feedback</h1>
-                    <p class="lg:w-2/3 mx-auto leading-relaxed text-base">If you have any questions or feedback about this provider please submit here. This will be passed along to the Best Buy team.</p>
+        <section className="text-gray-600 body-font relative">
+            <div className="container px-5 py-24 mx-auto">
+                <div className="flex flex-col text-center w-full mb-12">
+                    <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Feedback</h1>
+                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base">If you have any questions or feedback about this provider please submit here. This will be passed along to the Best Buy team.</p>
                 </div>
-                <div class="lg:w-1/2 md:w-2/3 mx-auto">
-                    <div class="flex flex-wrap -m-2">
-                        <div class="p-2 w-1/2">
-                            <div class="relative">
-                                <label htmlFor="name" class="leading-7 text-sm text-gray-600">Name</label>
-                                <input type="text" id="name" name="name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                <div className="lg:w-1/2 md:w-2/3 mx-auto">
+                    <div className="flex flex-wrap -m-2">
+                        <div className="p-2 w-1/2">
+                            <div className="relative">
+                                <label htmlFor="name" className="leading-7 text-sm text-gray-600">Name</label>
+                                <input type="text" id="name" name="name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                             </div>
                         </div>
-                        <div class="p-2 w-1/2">
-                            <div class="relative">
-                                <label htmlFor="email" class="leading-7 text-sm text-gray-600">Email</label>
-                                <input type="email" id="email" name="email" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                        <div className="p-2 w-1/2">
+                            <div className="relative">
+                                <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
+                                <input type="email" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                             </div>
                         </div>
-                        <div class="p-2 w-full">
-                            <div class="relative">
-                                <label htmlFor="message" class="leading-7 text-sm text-gray-600">Message</label>
-                                <textarea id="message" name="message" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                        <div className="p-2 w-full">
+                            <div className="relative">
+                                <label htmlFor="message" className="leading-7 text-sm text-gray-600">Message</label>
+                                <textarea id="message" name="message" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
                             </div>
                         </div>
-                        <div class="p-2 w-full">
-                            <button class="flex mx-auto text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">Button</button>
+                        <div className="p-2 w-full">
+                            <button className="flex mx-auto text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">Button</button>
                         </div>
-                        <div class="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
-                            <a class="text-blue-500" href="mailto:ApplianceInstallOps@bestbuy.com">ApplianceInstallOps@bestbuy.com</a>
-                            <p class="leading-normal my-5">7601 Penn Ave South
+                        <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
+                            <a className="text-blue-500" href="mailto:ApplianceInstallOps@bestbuy.com">ApplianceInstallOps@bestbuy.com</a>
+                            <p className="leading-normal my-5">7601 Penn Ave South
                                 <br />Richfield, MN 55423
                             </p>
                         </div>
--- app/routes/vendors/VendorList.jsx
+++ app/routes/vendors/VendorList.jsx
@@ -1,17 +1,28 @@
 import FeedBackForm from "~/Components/FeedbackForm";
 import Header from "~/Components/Header";
 import VendorCard from "~/Components/VendorCard";
+import superjson from 'superjson';
+import { db } from "~/utils/db.server";
+import { useLoaderData } from "@remix-run/react";
+
+export const loader = async ({ params }) => {
+    //search for vendor in database
+    const allVendors = await db.company.findMany();
+    //vendorCompanyObject =  findVendor(vendorCode)
+    //return vendor object in database
+    return superjson.stringify(allVendors);
+}
 
 const VendorList = () => {
+    const data = useLoaderData();
+    const vendorList = superjson.parse(data);
+    console.log(vendorList);
     return (<div>
         <Header />
         <section className="text-gray-600 body-font">
             <div className="container px-5 py-24 mx-auto">
                 <div className="flex flex-wrap -m-4">
-                    <VendorCard vendorName="Company1" />
-                    <VendorCard vendorName="Company2" />
-                    <VendorCard vendorName="Company3" />
-                    <VendorCard vendorName="Company4" />
+                    {vendorList.map((com) => { return (<VendorCard key={com.id} vendorName={com.name} vData={com} />) })}
                 </div>
             </div>
         </section>
